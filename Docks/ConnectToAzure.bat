8a79b9d237000e11a6c852750a38d865d1911c80
Kobus Jonker <kobus.jonker@sgits.co.za>
2017-05-30 21:47:36 +0200

    Update web.config to look at local db. Add connectionToAzure bat file to connect easy

---
+++ Docks/ConnectToAzure.bat
@@ -0,0 +1,10 @@
+echo off
+title Connect to Azure MySql Database
+
+
+cd\
+
+cd C:\Program Files\MySQL\MySQL Server 5.7\bin
+
+mysql -h jonkerbudget.mysql.database.azure.com -u azureroot@jonkerbudget -p jonkerbudget
+
